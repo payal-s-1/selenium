8eabaf48e5cdeb0a5068f7a4b8937f6f3816345f
payal-s-1 <M1089106@mindtree.com>
2022-06-29 23:27:18 +0530

    Committed HelloGit Project

---
+++ Project1/src/main/java/com/mindtree/seleniumclass/selenium1.java
@@ -0,0 +1,21 @@
+package com.mindtree.seleniumclass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium1 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("aurora");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//input[@class='gNO89b']")).click();
+	}
+
+}
+
